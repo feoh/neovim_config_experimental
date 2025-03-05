93919da0d3f1b9b5b25309d593b998e9a9d7717a
Chris Patti <feoh@feoh.org>
2025-03-05 06:22:27 -0500

    Add telescope ui picker config and copilot chat.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,33 @@
 -- plugins/telescope.lua:
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  opts = {
+    -- This is your opts table
+    require("telescope").setup {
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown {
+            -- even more opts
+          }
+
+          -- pseudo code / specification for writing custom displays, like the one
+          -- for "codeactions"
+          -- specific_opts = {
+          --   [kind] = {
+          --     make_indexed = function(items) -> indexed_items, width,
+          --     make_displayer = function(widths) -> displayer
+          --     make_display = function(displayer) -> function(e)
+          --     make_ordinal = function(e) -> string
+          --   },
+          --   -- for example to disable the custom builtin "codeactions" display
+          --      do the following
+          --   codeactions = false,
+          -- }
+        }
+      }
+    },
+    require("telescope").load_extension("ui-select")
+  },
   dependencies = {
     'nvim-lua/plenary.nvim',
      {
