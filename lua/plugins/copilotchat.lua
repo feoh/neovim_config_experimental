5338dcbd3f2658d5a617a2382669424af80c9123
Chris Patti <feoh@feoh.org>
2025-03-05 06:39:34 -0500

    Add needed opts for copilotchat

--- lua/plugins/copilotchat.lua
+++ lua/plugins/copilotchat.lua
@@ -6,5 +6,7 @@ return {
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
     build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+    },
   },
 }
