84b6c919b01d377448dd5b2c8564b8823cf7261d
Christopher Patti <feoh@feoh.org>
2025-09-14 17:25:27 -0400

    woops forgot to add file

---
+++ lua/config/ts-lsp-config.lua
@@ -0,0 +1,21 @@
+-- js/ts configuration
+local vue_language_server_path = vim.env['NVM_BIN']
+local vue_plugin = {
+  name = '@vue/typescript-plugin',
+  location = vue_language_server_path,
+  languages = { 'vue' },
+  configNamespace = 'typescript',
+}
+vim.lsp.config('vtsls', {
+  settings = {
+    vtsls = {
+      tsserver = {
+        globalPlugins = {
+          vue_plugin,
+        },
+      },
+    },
+  },
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+})
+
